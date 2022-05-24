e9e96c3f296c63c0d016597cc85176f9e3a99438
Ramon <ramon1234rxm@gmail.com>
2022-05-24 19:00:06 -0300

    feat: create a models for users and posts

---
+++ src/models/Post.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const Schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+export const Post = mongoose.model('Post', Schema);
