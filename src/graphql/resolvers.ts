aa5c58d1c5905f1720452003f22552f57be35f19
Ramon <ramon1234rxm@gmail.com>
2022-05-24 19:01:30 -0300

    feat: configure typeDefs and resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,1 +1,7 @@
-export const resolvers = {};
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeResolvers } from '@graphql-tools/merge';
+import { join } from 'path';
+
+const resolversArray = loadFilesSync(join(__dirname, 'modules/**/resolvers.ts'));
+
+export const resolvers = mergeResolvers(resolversArray);
--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,5 +1,7 @@
-export const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+import { join } from 'path';
+
+const typesArray = loadFilesSync(join(__dirname, 'modules/**/*.gql'));
+
+export const typeDefs = mergeTypeDefs(typesArray);
