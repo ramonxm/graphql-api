7bd632cdff29d0e66fda07901e8896bc911df605
Ramon <ramon1234rxm@gmail.com>
2022-05-24 19:01:05 -0300

    feat: make schemas and resolvers to users

---
+++ src/graphql/modules/users/resolvers.ts
@@ -0,0 +1,13 @@
+import { User } from '../../../models/User';
+
+export const resolvers = {
+  Query: {
+    users: async () => await User.find(),
+    user: async (_, { id }) => await User.findById(id),
+  },
+  Mutation: {
+    createUser: async (_, { data }) => await User.create(data),
+    updateUser: async (_, { id, data }) => await User.findByIdAndUpdate(id, data, { new: true }),
+    deleteUser: async (_, { id }) => Boolean(await User.findByIdAndDelete(id)),
+  },
+};
